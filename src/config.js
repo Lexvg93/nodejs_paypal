762ec1efd7b49ec3fa8e4f2a13f481c64c5639b1
Lexvg93 <alejandro_gonzalo92@icloud.com>
2023-05-12 14:33:57 +0200

    avance viernes 14:32

---
+++ src/config.js
@@ -0,0 +1,5 @@
+
+const PAYPAL_API_CLIENT = ''
+const PAYPAL_API_SECRET = ''
+
+https://www.youtube.com/watch?v=sBenKZqEzpQ tiempo 27:18
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import paymentRoutes from "./routes/payment.routes";
+import morgan from "morgan"
+
+const app = express();
+
+app.use(morgan('dev'))
+
+app.use(paymentRoutes)
+
+app.listen(3000);
+
+console.log("Server on port", 3000)
